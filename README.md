207e3a7f41ebbb029d69a7bca1114a0bb88934cf
5he1d0r <5he11d000r@gmail.com>
2020-06-18 11:22:22 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,11 @@
+# Use click-tt
+
+This is an user script for using click-tt instead of my-tischtennis
+
+## Information
+
+The script can handle all associations including the DTTV.
+
+##
+
+*The script is tested in [tampermonkey](https://www.tampermonkey.net/)*
---
+++ use-clicktt.js
@@ -0,0 +1,30 @@
+// ==UserScript==
+// @name         use click-tt
+// @namespace    http://tampermonkey.net/
+// @version      2.0.0
+// @description  avoid the usage of mytischtennis.de
+// @author       5he1d0r
+// @match        https://www.mytischtennis.de/*
+// @grant        none
+// ==/UserScript==
+
+const isValid = function(pathname) {
+    return pathname.includes('gruppe') &&
+        pathname.includes('tabelle');
+};
+
+(function() {
+    'use strict';
+
+    if(isValid(window.location.pathname)){
+        var aTag = document.getElementsByClassName('breadcrumb')[0].getElementsByTagName('a')[2];
+
+        var target = 'targetFed' + aTag.href.split('/')[2];
+
+        var championship = 'championship=' + aTag.title.split('(')[1].split(')')[0].split(' ').join('+').split('/').join('%2F');
+
+        var group = 'group=' + window.location.pathname.split('/')[7];
+
+        location.replace('https://wttv.click-tt.de/cgi-bin/WebObjects/nuLigaTTDE.woa/wa/groupPage?' + target + '&' + championship + '&' + group);
+    }
+})();
